a9b39254a2784d89b4a550641e8293d71fa1cb68
hyunminyoo <67991188+hyunminyoo@users.noreply.github.com>
2021-01-07 17:35:16 +0900

    fixed login player issue. Trying to add pose detector to the YogaVideo. Yet camera access problem unsolved.

--- app/src/main/java/com/growin/silveryogaapp/YogaVideo.java
+++ app/src/main/java/com/growin/silveryogaapp/YogaVideo.java
@@ -2,6 +2,9 @@ package com.growin.silveryogaapp;
 
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
@@ -39,6 +42,9 @@ public class YogaVideo extends YouTubeBaseActivity {
     private String pName;
     private int stateFavorit;
 
+    private WebView mWebView; // 웹뷰 선언
+    private WebSettings mWebSettings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,6 +60,29 @@ public class YogaVideo extends YouTubeBaseActivity {
         playBtn = findViewById(R.id.youtubeBtn);
         likeBtn = findViewById(R.id.likeBtn);
         playerView = findViewById(R.id.youtubeView);
+
+
+        //Webview for checking pose accuracy
+        mWebView = (WebView) findViewById(R.id.webView);
+
+
+
+        mWebView.setWebViewClient(new WebViewClient()); // 클릭시 새창 안뜨게
+        mWebSettings = mWebView.getSettings(); //세부 세팅 등록
+        mWebSettings.setJavaScriptEnabled(true); // 웹페이지 자바스클비트 허용 여부
+        mWebSettings.setSupportMultipleWindows(false); // 새창 띄우기 허용 여부
+        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(false); // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
+        mWebSettings.setLoadWithOverviewMode(true); // 메타태그 허용 여부
+        mWebSettings.setUseWideViewPort(true); // 화면 사이즈 맞추기 허용 여부
+        mWebSettings.setSupportZoom(false); // 화면 줌 허용 여부
+        mWebSettings.setBuiltInZoomControls(false); // 화면 확대 축소 허용 여부
+        mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN); // 컨텐츠 사이즈 맞추기
+        mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
+        mWebSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
+
+        mWebView.loadUrl("https://silversquat.netlify.app/"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
+
+
 
         CheckFavoritVideo(pMail, pVideo, new IYogaVideo() {
             @Override
